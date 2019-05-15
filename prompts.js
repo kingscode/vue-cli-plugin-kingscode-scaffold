da582ea41584764793caedc8f98308f0db6d1d21
ricardovanlaarhoven <ricardo1994@gmail.com>
2019-05-15 10:00:29 +0200

    added prompts for env file generation

--- prompts.js
+++ prompts.js
@@ -1,3 +1,7 @@
+function useAuthorisation (answers) {
+    return answers.useAuthorisation
+}
+
 module.exports = [
     {
         name: 'useCrud',
@@ -16,5 +20,23 @@ module.exports = [
         type: 'confirm',
         message: 'Do you want to use authorisation (Login/register/invitation/password forgotten)',
         default: true,
+    },
+    {
+        name: 'oauthClientId',
+        type: 'input',
+        message: 'What is the oauth client id? (run php artisan passport:client --password (you can change this later)',
+        when: useAuthorisation
+    },
+    {
+        name: 'oauthSecret',
+        type: 'input',
+        message: 'What is the oauth client secret? (run php artisan passport:client --password (you can change this later)',
+        when: useAuthorisation
+    },
+    {
+        name: 'rootUrl',
+        type: 'input',
+        message: 'What is the root API url? (you can change this later)',
+        default: '//example.local/api',
     }
 ];
