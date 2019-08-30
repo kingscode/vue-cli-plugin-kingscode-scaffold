98381978c87439dd494406424f3442ec9fb8db67
ricardovanlaarhoven <ricardo1994@gmail.com>
2019-08-30 11:40:24 +0200

    added skipWating to vue config for PWA'S

---
+++ generator/templates/Default/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    publicPath: process.env.VUE_APP_PUBLIC_PATH,
+    pwa: {
+        workboxPluginMode: 'GenerateSW',
+        workboxOptions: {
+            skipWaiting: true,
+        },
+    },
+};
