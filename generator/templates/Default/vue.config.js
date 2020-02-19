319714c5bef5b0bd1fa3524b1add2a54b643b255
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-02-19 13:51:47 +0100

    update htaccess and vue config (for ignoring htaccess while compiling pwa)

--- generator/templates/Default/vue.config.js
+++ generator/templates/Default/vue.config.js
@@ -7,6 +7,10 @@ module.exports = {
         workboxPluginMode: 'GenerateSW',
         workboxOptions: {
             skipWaiting: true,
+            clientsClaim: true,
+            exclude: [
+                /\.htaccess$/
+            ]
         },
     },
 };
