fe06cc766e274d6c6f345b507e657a936e5ae049
ricardovanlaarhoven <ricardo1994@gmail.com>
2020-05-25 13:53:11 +0200

    add some sentry deployment variables

--- generator/templates/Plugins/sentry/src/plugins/sentry.js
+++ generator/templates/Plugins/sentry/src/plugins/sentry.js
@@ -4,6 +4,8 @@ import Vue from 'vue';
 
 if (process.env.VUE_APP_SENTRY) {
     Sentry.init({
+        environment: process.env.VUE_APP_ENV,
+        release: process.env.VUE_APP_RELEASE,
         dsn: process.env.VUE_APP_SENTRY,
         integrations: [new Integrations.Vue({Vue, attachProps: true})],
     });
