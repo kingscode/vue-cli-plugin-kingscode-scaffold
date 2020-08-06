8abfe3f82f275a7958f5f9d57b98a51f6d5b488f
DeluzeDEV <bartramaekers99@gmail.com>
2020-08-06 12:11:30 +0200

    Update model.js value mapping

--- generator/templates/Default/src/application/models/model.js
+++ generator/templates/Default/src/application/models/model.js
@@ -2,7 +2,6 @@
  *
  */
 class Model {
-
   /**
    * @return {{}};
    */
@@ -12,10 +11,66 @@ class Model {
 
   /**
    * @param data {{}}
+   * @return {model}
    */
   mapResponse(data) {
-    throw new Error(`${this.constructor.name}::mapResponse unset, keys: ${Object.keys(data)
-      .join(', ')}`);
+    return this.initializeObjectFromApi(data, this);
+  }
+
+  /**
+   * @private
+   * @param data
+   * @param assignable
+   * @return {model}
+   */
+  initializeObjectFromApi(data, assignable) {
+    Object.keys(assignable)
+      .forEach(key => {
+        const value = assignable[key];
+        const type = typeof value;
+
+        if (typeof data[key] === type) {
+          assignable[key] = data[key];
+          return;
+        }
+
+        const defaultValue = this.getDefaultValueFromType(type, value);
+
+        assignable[key] = data[key] || defaultValue;
+      });
+
+    Object.keys(data)
+      .forEach(key => {
+        if (typeof assignable[key] === 'undefined') {
+          assignable[key] = data[key];
+        }
+      });
+
+    return assignable;
+  }
+
+  /**
+   * @private
+   * @param type
+   * @param defaultModelValue
+   * @return {*}
+   */
+  getDefaultValueFromType(type, defaultModelValue) {
+    let defaultValue;
+
+    if (type === 'string') {
+      defaultValue = defaultModelValue || '';
+    } else if (type === 'boolean') {
+      defaultValue = defaultModelValue || false;
+    } else if (type === 'number') {
+      defaultValue = defaultModelValue || 0;
+    } else if (Array.isArray(defaultModelValue)) {
+      defaultValue = defaultModelValue || [];
+    } else {
+      defaultValue = defaultModelValue || null;
+    }
+
+    return defaultValue;
   }
 }
 
--- generator/templates/Default/src/application/models/user.js
+++ generator/templates/Default/src/application/models/user.js
@@ -11,12 +11,6 @@ class User extends Model {
       email: this.email,
     };
   }
-
-  mapResponse(data) {
-    this.id = data.id;
-    this.name = data.name;
-    this.email = data.email;
-  }
 }
 
 export default User;
