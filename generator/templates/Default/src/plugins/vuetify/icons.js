d4bc5288857ded87921ac6cc9f84daad6953b78b
Jens van Hellemondt <jens@kingscode.nl>
2020-12-11 10:52:53 +0100

    :sparkles: Free and Pro SVG icons

---
+++ generator/templates/Default/src/plugins/vuetify/icons.js
@@ -0,0 +1,7 @@
+<%_ if (options.plugins.includes('fontawesomepro')){ _%>
+import icons from './FontawesomePro'
+<%_ } else { _%>
+import icons from './FontawesomeFree'
+<%_ } _%>
+
+export default icons
