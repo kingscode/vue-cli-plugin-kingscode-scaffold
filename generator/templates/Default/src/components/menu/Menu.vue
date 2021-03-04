95c4b053011aedafac24d51cbf2ba02521de5fa6
ricardovanlaarhoven <ricardo1994@gmail.com>
2021-03-04 11:21:02 +0100

    fix

--- generator/templates/Default/src/components/menu/Menu.vue
+++ generator/templates/Default/src/components/menu/Menu.vue
@@ -10,9 +10,6 @@
       >
 
         <template #activator>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
           <v-list-item-content>
             <v-list-item-title v-text="item.title"></v-list-item-title>
           </v-list-item-content>
