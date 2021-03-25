9f266d4aaea66f76841874b95b5873b8adf53971
Jens van Hellemondt <jens.vanhellemondt@gmail.com>
2021-03-25 11:24:19 +0100

    refactor: removed unnecessary icon

--- generator/templates/Default/src/plugins/vuetify/icons/application.js
+++ generator/templates/Default/src/plugins/vuetify/icons/application.js
@@ -1,17 +1,14 @@
 import {
   faBox,
-  faFolderTree
 } from '<%_ if (options.plugins.includes("fontawesomepro")){ _%> @fortawesome/pro-solid-svg-icons<%_ } else { _%> @fortawesome/free-solid-svg-icons<%_ } _%>';
 
 const icons = [
  faBox,
-  faFolderTree
 ];
 
 /*project specific icons*/
 const identifiers = {
   product: ['fas', 'box'],
-  productCategories: ['fas', 'folder-tree']
 };
 
 export default {
